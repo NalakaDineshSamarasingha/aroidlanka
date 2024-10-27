db27378301d8c47ef038f9a5ff8ed801bdd88d4f
NalakaDineshSamarasingha <nalakadinesh97@gmail.com>
2024-10-27 19:28:17 +0530

    add page loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,46 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Test from './pages/Test';
-import Nav from './components/Navbar/Nav';
-import Footer from './components/Footer/Footer';
-import ProductPage from './components/ProductPage/ProductPage';
+import ScaleLoader from "react-spinners/ScaleLoader";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
+import Nav from "./components/Navbar/Nav";
+import Footer from "./components/Footer/Footer";
+import ProductPage from "./components/ProductPage/ProductPage";
+import { useEffect, useState } from "react";
 
 function App() {
-
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 7000);
+  }, []);
   return (
     <div className="App">
-      <BrowserRouter>
-      <Nav/>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="blogs" element={<Test />} />
-        <Route path="contact" element={<Test />} />
-        <Route path="*" element={<Test />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-
+      {loading ? (
+        <div className="loader">
+          <ScaleLoader
+            color={"#2eb82e"}
+            loading={loading}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="blogs" element={<Test />} />
+            <Route path="contact" element={<Test />} />
+            <Route path="*" element={<Test />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </div>
   );
 }
